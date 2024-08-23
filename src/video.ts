5d61a2b841abf18441b72514ce20c337a4ae76d3
dev2820 <second28200@naver.com>
2024-08-23 23:53:04 +0900

    fix: use back camera

--- src/video.ts
+++ src/video.ts
@@ -3,7 +3,12 @@ export async function startWebcam() {
   const video = document.getElementById("webcam") as HTMLVideoElement;
   try {
     // 웹캠 스트림을 요청
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: "environment",
+      },
+    });
     video.srcObject = stream;
   } catch (err) {
     console.error("웹캠을 불러오는 중 오류 발생:", err);
